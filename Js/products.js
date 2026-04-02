d41c8fac4ae80b76e9624636520f967032c744d1
Francodarre <franqui6767@gmail.com>
2026-04-02 19:36:40 -0300

    Actualizo el js de products, el html de ofertas y prodcutos, y el scss de carrito

--- Js/products.js
+++ Js/products.js
@@ -1,11 +1,74 @@
-let btnAgregar = document.querySelectorAll(".btn-agregar");
+document.addEventListener("click", (e) => {
 
-btnAgregar.forEach(boton => {
-    boton.addEventListener("click", () => {
+    if (e.target.classList.contains("btn-agregar")) {
 
-        let nombre = boton.dataset.nombre;
-        let precio = parseInt(boton.dataset.precio);
+        let nombre = e.target.dataset.nombre;
+        let precio = parseInt(e.target.dataset.precio);
 
         agregarAlCarrito(nombre, precio);
-    });
+    }
 });
+
+async function cargarProductos(){
+
+    try {
+
+        let res = await fetch("https://sheetdb.io/api/v1/ers2l3iahuwip");
+        let productos = await res.json();
+
+        renderCategoria(productos, "suplementacion", "#suplementacion");
+        renderCategoria(productos, "musculosas", "#musculosas");
+        renderCategoria(productos, "remeras", "#remeras");
+        renderCategoria(productos, "buzos", "#buzos");
+        renderCategoria(productos, "medias", "#medias");
+
+    } catch (error) {
+        console.error("Error cargando productos", error);
+    }
+}
+
+function renderCategoria(productos, categoria, selector){
+
+    let container = document.querySelector(selector);
+
+    if(!container) return;
+
+    container.innerHTML = "";
+
+    let filtrados = productos.filter(p => p.categoria === categoria);
+
+    filtrados.forEach(prod => {
+
+        container.innerHTML += `
+            <div class="producto">
+                <img src="${prod.imagen}" alt="${prod.nombre}">
+                <p>$${prod.precio}</p>
+
+                <button class="btn-agregar"
+                    data-nombre="${prod.nombre}"
+                    data-precio="${parseInt(prod.precio)}">
+                    Agregar
+                </button>
+            </div>
+        `;
+    });
+}
+
+cargarProductos();
+
+if (!localStorage.getItem("promoMostrada")) {
+
+    Swal.fire({
+        title: "🔥 PROMO ACTIVA",
+        html: "¡Hasta el <b>20 de mayo</b>!<br>Superando $70.000 tenés <b>ENVÍO GRATIS</b>",
+        icon: "success",
+        confirmButtonText: "Buenísimo",
+        background: "#eb4a00",
+        color: "#000",
+        confirmButtonColor: "#000"
+    });
+
+    localStorage.setItem("promoMostrada", "true");
+}
+
+localStorage.removeItem("promoMostrada");
